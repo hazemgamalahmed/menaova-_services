20c760bf827be7ff2af86ec4b3465f9e4cb6a92f
Mahmoud AbdElsalam <mahmoud.afg19@gmail.com>
2020-05-16 20:27:30 +0200

    menova v2

--- main.js
+++ main.js
@@ -15,3 +15,25 @@ landingPage.style.backgroundImage = 'url("assets/'+ myImages[randomeNumber]+'")'
 var myNavLink = document.querySelectorAll(".nav-item");
 
 // console.log(myNavLink);
+
+
+
+// the function of fixed header
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementById("main-navbar").style.padding = "10px 10px";
+    document.getElementById("main-navbar").style.top = 0;
+    document.getElementById("main-navbar").style.background = " rgba(0,0,0, .9)";
+  } else {
+  	document.getElementById("main-navbar").style.padding = "50px 10px";
+    document.getElementById("main-navbar").style.top = "45px";
+    document.getElementById("main-navbar").style.background = " rgba(0,0,0, .8)";
+  }
+}
+
+// onreload function
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
